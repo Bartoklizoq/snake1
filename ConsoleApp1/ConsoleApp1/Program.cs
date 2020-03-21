aed0f6442dd9952c6c5beac35be5cdf30691f992
Sanchupan <sanchezurus@gmail.com>
2020-03-21 06:30:18 +0300

    qssad

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,15 +10,31 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.SetWindowSize(1, 1);
+            Console.SetBufferSize(80, 25);
+            Console.SetWindowSize(80, 25);
 
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            // отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
 
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Drow();
+
+           
+
+            Point p = new Point(4, 5, '#');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
+            //HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            //line.Drow();
+
+            
 
             Console.ReadKey();
         }
